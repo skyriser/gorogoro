e850c185caa06d80a798657059f08a8224f2ac78
myun2 <myun2@nwhite.info>
2016-01-12 08:24:27 +0000

    Add "おはよう", "おやすみ" response

--- ikaduti.rb
+++ ikaduti.rb
@@ -1,6 +1,12 @@
 require 'slack-ruby-bot'
 
 class Ikaduti < SlackRubyBot::Bot
+  command 'おはよう' do |client, data, match|
+    client.say(text: '＜おはよう司令官！今日も一緒に頑張りましょー！', channel: data.channel)
+  end
+  command 'おやすみ' do |client, data, match|
+    client.say(text: '＜おやすみ司令官！もーっと私に頼っていいのよ？', channel: data.channel)
+  end
   command 'ゴロゴロ' do |client, data, match|
     client.say(text: '＜司令官、私がいるじゃない！', channel: data.channel)
   end
