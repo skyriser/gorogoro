b194bf9f82b7edfe7e67f5d558df99f61ce58442
myun2 <myun2@nwhite.info>
2016-01-14 04:46:28 +0000

    Update ikaduti

--- ikaduti.rb
+++ ikaduti.rb
@@ -2,13 +2,34 @@ require 'slack-ruby-bot'
 
 class Ikaduti < SlackRubyBot::Bot
   command 'おはよう' do |client, data, match|
-    client.say(text: '＜おはよう司令官！今日も一緒に頑張りましょー！', channel: data.channel)
+    client.say(text: '＜おはよう、司令官！今日も一日頑張りましょー？', channel: data.channel)
   end
   command 'おやすみ' do |client, data, match|
-    client.say(text: '＜おやすみ司令官！もーっと私に頼っていいのよ？', channel: data.channel)
+    client.say(text: '＜おやすみなさい！司令官！ゆーーっくり、休むといいわ！', channel: data.channel)
   end
-  command 'ゴロゴロ' do |client, data, match|
-    client.say(text: '＜司令官、私がいるじゃない！', channel: data.channel)
+  command 'ゴロゴロ', 'ごろごろ' do |client, data, match|
+    s = [
+      '＜司令官、私がいるじゃない！',
+      '＜雷よ！かみなりじゃないわ！そこのとこもよろしく頼むわねっ！',
+      '＜元気ないわねーそんなんじゃ駄目よぉ！',
+      '＜そうそう。もーっと私に頼っていいのよ！',
+      '＜はーい！司令官。行っきますよー！',
+      '＜雷、司令官のために出撃しちゃうねっ',
+      'なによもう、雷は大丈夫なんだから！'
+    ]
+    client.say(text: s.sample, channel: data.channel)
+  end
+  command 'ランチ', 'はらへ', 'お腹空いた', 'おなかすいた' do |client, data, match|
+    s = [
+      '＜もう、さっき食べたでしょ！',
+      '＜ゴロゴロー。',
+      '＜っ高速修復材',
+      '＜っボーキサイト',
+      '＜さっき魚を釣って来たわ。一緒に食べましょう？',
+      '＜おなかが空いたなら、お肉を食べればいいじゃない！',
+      '＜金曜日はカレーを食べるといいと思うわ！',
+    ]
+    client.say(text: s.sample, channel: data.channel)
   end
 end
 
